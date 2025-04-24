1c22ce85595e940657beb3f980604d727dd20373
ricval <rcvaldes@gmail.com>
2025-04-24 13:03:13 -0600

    Migración del Módulo de Módulos

--- tauro/blueprints/modulos/views.py
+++ tauro/blueprints/modulos/views.py
@@ -11,7 +11,7 @@ from lib.datatables import get_datatable_parameters, output_datatable_json
 from lib.safe_string import safe_message, safe_string
 from tauro.blueprints.bitacoras.models import Bitacora
 
-# from tauro.blueprints.modulos.forms import ModuloForm
+from tauro.blueprints.modulos.forms import ModuloForm
 from tauro.blueprints.modulos.models import Modulo
 from tauro.blueprints.permisos.models import Permiso
 from tauro.blueprints.usuarios.decorators import permission_required
@@ -35,8 +35,6 @@ def datatable_json():
     draw, start, rows_per_page = get_datatable_parameters()
     # Consultar
     consulta = Modulo.query
-    # Solo los modulos en Plataforma Hercules
-    # consulta = consulta.filter_by(en_plataforma_hercules=True)
     # Primero filtrar por columnas propias
     if "estatus" in request.form:
         consulta = consulta.filter_by(estatus=request.form["estatus"])
@@ -94,3 +92,135 @@ def detail(modulo_id):
     """Detalle de un Modulo"""
     modulo = Modulo.query.get_or_404(modulo_id)
     return render_template("modulos/detail.jinja2", modulo=modulo)
+
+
+@modulos.route("/modulos/nuevo", methods=["GET", "POST"])
+@permission_required(MODULO, Permiso.CREAR)
+def new():
+    """Nuevo Modulo"""
+    form = ModuloForm()
+    if form.validate_on_submit():
+        # Validar que el nombre no se repita
+        nombre = safe_string(form.nombre.data, save_enie=True)
+        if Modulo.query.filter_by(nombre=nombre).first():
+            flash("El nombre ya está en uso. Debe de ser único.", "warning")
+            return render_template("modulos/new.jinja2", form=form)
+        # Guardar
+        modulo = Modulo(
+            nombre=nombre,
+            nombre_corto=safe_string(form.nombre_corto.data, do_unidecode=False, to_uppercase=False, save_enie=True),
+            icono=form.icono.data,
+            ruta=form.ruta.data,
+            en_navegacion=form.en_navegacion.data,
+        )
+        modulo.save()
+        bitacora = Bitacora(
+            modulo=Modulo.query.filter_by(nombre=MODULO).first(),
+            usuario=current_user,
+            descripcion=safe_message(f"Nuevo Modulo {modulo.nombre}"),
+            url=url_for("modulos.detail", modulo_id=modulo.id),
+        )
+        bitacora.save()
+        flash(bitacora.descripcion, "success")
+        return redirect(bitacora.url)
+    return render_template("modulos/new.jinja2", form=form)
+
+
+@modulos.route("/modulos/edicion/<int:modulo_id>", methods=["GET", "POST"])
+@permission_required(MODULO, Permiso.MODIFICAR)
+def edit(modulo_id):
+    """Editar Modulo"""
+    modulo = Modulo.query.get_or_404(modulo_id)
+    form = ModuloForm()
+    if form.validate_on_submit():
+        es_valido = True
+        # Si cambia el nombre verificar que no este en uso
+        nombre = safe_string(form.nombre.data, save_enie=True)
+        if modulo.nombre != nombre:
+            modulo_existente = Modulo.query.filter_by(nombre=nombre).first()
+            if modulo_existente and modulo_existente.id != modulo.id:
+                es_valido = False
+                flash("El nombre ya está en uso. Debe de ser único.", "warning")
+        # Si es valido actualizar
+        if es_valido:
+            modulo.nombre = nombre
+            modulo.nombre_corto = safe_string(form.nombre_corto.data, do_unidecode=False, to_uppercase=False, save_enie=True)
+            modulo.icono = form.icono.data
+            modulo.ruta = form.ruta.data
+            modulo.en_navegacion = form.en_navegacion.data
+            modulo.save()
+            bitacora = Bitacora(
+                modulo=Modulo.query.filter_by(nombre=MODULO).first(),
+                usuario=current_user,
+                descripcion=safe_message(f"Editado Modulo {modulo.nombre}"),
+                url=url_for("modulos.detail", modulo_id=modulo.id),
+            )
+            bitacora.save()
+            flash(bitacora.descripcion, "success")
+            return redirect(bitacora.url)
+    form.nombre.data = modulo.nombre
+    form.nombre_corto.data = modulo.nombre_corto
+    form.icono.data = modulo.icono
+    form.ruta.data = modulo.ruta
+    form.en_navegacion.data = modulo.en_navegacion
+    return render_template("modulos/edit.jinja2", form=form, modulo=modulo)
+
+
+@modulos.route("/modulos/eliminar/<int:modulo_id>")
+@permission_required(MODULO, Permiso.ADMINISTRAR)
+def delete(modulo_id):
+    """Eliminar Modulo"""
+    este_modulo = Modulo.query.get_or_404(modulo_id)
+    if este_modulo.estatus == "A":
+        # Dar de baja el modulo
+        este_modulo.delete()
+        # Dar de baja los permisos asociados
+        for permiso in este_modulo.permisos:
+            permiso.delete()
+        # Guardar en la bitacora
+        bitacora = Bitacora(
+            modulo=Modulo.query.filter_by(nombre=MODULO).first(),
+            usuario=current_user,
+            descripcion=safe_message(f"Eliminado Modulo {este_modulo.nombre}"),
+            url=url_for("modulos.detail", modulo_id=este_modulo.id),
+        )
+        bitacora.save()
+        flash(bitacora.descripcion, "success")
+    return redirect(url_for("modulos.detail", modulo_id=este_modulo.id))
+
+
+@modulos.route("/modulos/recuperar/<int:modulo_id>")
+@permission_required(MODULO, Permiso.ADMINISTRAR)
+def recover(modulo_id):
+    """Recuperar Modulo"""
+    este_modulo = Modulo.query.get_or_404(modulo_id)
+    if este_modulo.estatus == "B":
+        # Dar de alta el modulo
+        este_modulo.recover()
+        # Dar de alta los permisos asociados
+        for permiso in este_modulo.permisos:
+            permiso.recover()
+        # Guardar en la bitacora
+        bitacora = Bitacora(
+            modulo=Modulo.query.filter_by(nombre=MODULO).first(),
+            usuario=current_user,
+            descripcion=safe_message(f"Recuperado Modulo {este_modulo.nombre}"),
+            url=url_for("modulos.detail", modulo_id=este_modulo.id),
+        )
+        bitacora.save()
+        flash(bitacora.descripcion, "success")
+    return redirect(url_for("modulos.detail", modulo_id=este_modulo.id))
+
+
+@modulos.route("/modulos/select2_json", methods=["POST"])
+def select2_json():
+    """Proporcionar el JSON de modulos para elegir con un Select2, se usa para filtrar en DataTables"""
+    consulta = Modulo.query.filter(Modulo.estatus == "A")
+    if "searchString" in request.form:
+        nombre = safe_string(request.form["searchString"], save_enie=True)
+        if nombre != "":
+            consulta = consulta.filter(Modulo.nombre.contains(nombre))
+    resultados = []
+    for modulo in consulta.order_by(Modulo.nombre).limit(10).all():
+        resultados.append({"id": modulo.id, "text": modulo.nombre})
+    return {"results": resultados, "pagination": {"more": False}}
