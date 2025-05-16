1d856393c26e5bc8410e11a541df740f5451e257
Guillermo Valdes <guillermo@movimientolibre.com>
2025-05-16 15:24:14 -0600

    CORS listos

--- tauro/blueprints/api_v1/resources.py
+++ tauro/blueprints/api_v1/resources.py
@@ -3,8 +3,10 @@ API v1 Resources
 """
 
 from flask import Blueprint
+from flask_cors import CORS
 from flask_restful import Api
 
+from config.settings import get_settings
 from tauro.blueprints.api_v1.endpoints.actualizar_turno_estado import ActualizarTurnoEstado
 from tauro.blueprints.api_v1.endpoints.actualizar_usuario import ActualizarUsuario
 from tauro.blueprints.api_v1.endpoints.autenticar import Authenticate
@@ -17,9 +19,13 @@ from tauro.blueprints.api_v1.endpoints.consultar_ventanillas_activas import Cons
 from tauro.blueprints.api_v1.endpoints.crear_turno import CrearTurno
 from tauro.blueprints.api_v1.endpoints.tomar_turno import TomarTurno
 
+settings = get_settings()
+
 api_v1 = Blueprint("api_v1", __name__, url_prefix="/api/v1")
 
 api = Api(api_v1)
+CORS(api_v1)
+CORS(api_v1, origins=[settings.HOST])
 
 
 api.add_resource(Authenticate, "/token")
